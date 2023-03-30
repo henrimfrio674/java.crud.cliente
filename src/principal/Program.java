b19bf4259a896893aaf0ba5b0c9236238f19666a
henrimfrio674 <erickalgun82@gmail.com>
2023-03-30 13:17:28 -0300

    Desenvolvimento dos repositórios e controladores

--- src/principal/Program.java
+++ src/principal/Program.java
@@ -1,16 +1,42 @@
 package principal;
 
+import java.util.Scanner;
+
 import controllers.ClienteRController;
 
 public class Program {
 
 	public static void main(String[] args) {
+		System.out.println("{1} CADASTRAR");
+		System.out.println("{2} ATUALIZAR");
+		System.out.println("{3} EXCLUIR");
+		System.out.println("{4} VER");
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("\nEscolha :");
+		Integer opcao = Integer.parseInt(scanner.nextLine());
 
 		ClienteRController clienteController = new ClienteRController();
-		clienteController.cadastrarCliente();
+		switch (opcao) {
+		case 1:
+			clienteController.cadastrarCliente();
+			break;
+
+		case 2:
+			clienteController.atualizarCliente();
+			break;
+
+		case 3:
+			clienteController.excluirCliente();
+			break;
+		case 4:
+			clienteController.consultarClientes();
+			break;
+		default:
+			System.out.println("\nOpção inválida.");
+		}
+
 	}
 
 }
-
-
-
--- src/repositories/ClienteRepository.java
+++ src/repositories/ClienteRepository.java
@@ -2,6 +2,9 @@ package repositories;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import entities.Clientes;
 import factories.ConectionFactory;
@@ -16,7 +19,8 @@ public class ClienteRepository {
 		Connection connection = connectionFactory.getConnection();
 		
 		//escrevendo um comando em linguagem SQL para inserir um cliente no banco de dados
-		PreparedStatement statement = connection.prepareStatement("insert into cliente(nome, email, telefone, cpf) values(?,?,?,?)");
+		PreparedStatement statement = connection.prepareStatement
+				("insert into cliente(nome, email, telefone, cpf) values(?,?,?,?)");
 		statement.setString(1, cliente.getNome());
 		statement.setString(2, cliente.getEmail());
 		statement.setString(3, cliente.getTelefone());
@@ -26,7 +30,80 @@ public class ClienteRepository {
 		//fechando a conexão
 		connection.close();
 	}		
+	
+	//método para receber e atualizar um cliente no banco de dados
+	public void update(Clientes cliente) throws Exception {
+		
+		//abrindo conexão com o banco de dados
+		ConectionFactory connectionFactory = new ConectionFactory();
+		Connection connection = connectionFactory.getConnection();
+		
+		//escrevendo um comando em linguagem SQL para atualizar um cliente no banco de dados
+		PreparedStatement statement = connection.prepareStatement
+				("update cliente set nome=?, email=?, telefone=?, cpf=? where idcliente=?");
+		statement.setString(1, cliente.getNome());
+		statement.setString(2, cliente.getEmail());
+		statement.setString(3, cliente.getTelefone());
+		statement.setString(4, cliente.getCpf());
+		statement.setInt(5, cliente.getIdCliente());
+		statement.execute();
+		
+		//fechando a conexão
+		connection.close();
+	}	
+	
+	//método para receber e excluir um cliente no banco de dados
+	public void delete(Clientes cliente) throws Exception {
+		
+		//abrindo conexão com o banco de dados
+		ConectionFactory connectionFactory = new ConectionFactory();
+		Connection connection = connectionFactory.getConnection();
+		
+		//escrevendo um comando em linguagem SQL para excluir um cliente no banco de dados
+		PreparedStatement statement = connection.prepareStatement
+				("delete from cliente where idcliente=?");
+		statement.setInt(1, cliente.getIdCliente());
+		statement.execute();
+		
+		connection.close();
+	}	
+	
+	//método para consultar todos os clientes cadastrados no banco de dados
+	public List<Clientes> findAll() throws Exception {
+		
+		//abrinco conexão com o banco de dados
+		ConectionFactory connectionFactory = new ConectionFactory();
+		Connection connection = connectionFactory.getConnection();
+		
+		//escrevendo um comando em linguagem SQL para consultar todos os clientes
+		PreparedStatement statement = connection.prepareStatement("select * from cliente");
+		ResultSet resultSet = statement.executeQuery();
+		
+		//declarando uma listagem de clientes
+		List<Clientes> lista = new ArrayList<Clientes>();
+		
+		//percorrendo cada registro obtido do banco de dados
+		while(resultSet.next()) {
+			
+			Clientes cliente = new Clientes();
+			
+			//capturando os campos do banco de dados
+			cliente.setIdCliente(resultSet.getInt("idcliente"));
+			cliente.setNome(resultSet.getString("nome"));
+			cliente.setEmail(resultSet.getString("email"));
+			cliente.setTelefone(resultSet.getString("telefone"));
+			cliente.setCpf(resultSet.getString("cpf"));
+			
+			//adicionar cada objeto cliente dentro da lista
+			lista.add(cliente);
+		}
+		
+		//fechando conexão
+		connection.close();
+		return lista; //retornando a listagem de clientes
+	}	
 }
+
 
 
 
